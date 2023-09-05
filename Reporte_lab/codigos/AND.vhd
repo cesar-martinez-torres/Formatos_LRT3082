5718afe745afa9206b02ca90cc8c918b3b91cb30
Cesar Martinez Torres <20808@udla.fundacion.mx>
2023-09-05 13:48:05 -0600

    Lab

---
+++ Reporte_lab/codigos/AND.vhd
@@ -0,0 +1,15 @@
+-- Simple AND gate design
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity and_gate is
+port(
+  a in std_logic;
+  b in std_logic;
+  q out std_logic);
+end and_gate;
+
+architecture rtl of and_gate is
+begin
+    q = a and b;
+end rtl;
