7f445dbe062e2c5483ec76d4bb5a5d66ac6f0193
Cesar Martinez Torres <20808@udla.fundacion.mx>
2023-09-26 12:17:40 -0600

    26 septiembre

---
+++ Codigos_clase/Unidad_2/sep26.m
@@ -0,0 +1,8 @@
+%% Ejercicio
+Num=18;
+Den=[1, 2.4, 9];
+G=tf(Num,Den)
+% Analice el sistema tipo, polos, cuanto vale wn, zeta y K 
+%% Diseñe un sistema sobre amortiguado con polos en -2 y -13
+%% Diseñe un sistema de segundo orden oscilatorio
+%% 
